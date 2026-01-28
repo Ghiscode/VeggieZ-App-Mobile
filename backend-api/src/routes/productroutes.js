7caf4dc9ca39965b9cffde8d3ff87d1f89b63d82
Mughis Fadhil A. Ridwan <mughisfar@gmail.com>
2026-01-28 13:00:56 +0700

    resetup deleted prisma

--- backend-api/src/routes/productroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const productController = require("../controllers/productController");
-
-// Kalau ada yang minta GET /api/products, panggil fungsi ini:
-router.get("/", productController.getAllProducts);
-
-module.exports = router;
